86e4cc0cfd97ab10339043ecd0c481810bcf3158
Alankrit Verma <alankritverma.lko@gmail.com>
2021-05-10 00:17:42 -0700

    coins

---
+++ nonConstructibleChange.js
@@ -0,0 +1,11 @@
+//o(nlogn)T & o(1)S
+function nonConstructibleChange(coins) {
+  // Write your code here.
+  coins.sort((a, b) => a - b);
+  let change = 0;
+  for (let num of coins) {
+    if (num > change + 1) return change + 1;
+    change = change + num;
+  }
+  return change + 1;
+}
